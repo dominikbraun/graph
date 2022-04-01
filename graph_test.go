333948f0b5968226f2880f7785babd1480ce26ee
Dominik Braun <dominik.braun@chefkoch.de>
2022-04-01 12:07:59 +0200

    Create tests for Graph.Vertices and Graph.Edges

--- graph_test.go
+++ graph_test.go
@@ -21,16 +21,84 @@ func TestSet_Values(t *testing.T) {
 		},
 	}
 
+	equals := func(a, b int) bool {
+		return a == b
+	}
+
 	for name, test := range tests {
 		actual := test.set.Values()
 
-		if !slicesAreEqual(actual, test.expected) {
+		if !slicesAreEqual(actual, test.expected, equals) {
 			t.Fatalf("%s: expected %v, got %v", name, test.expected, actual)
 		}
 	}
 }
 
-func slicesAreEqual[T comparable](a []T, b []T) bool {
+func TestGraph_Vertices(t *testing.T) {
+	tests := map[string]struct {
+		graph    Graph[int]
+		expected []int
+	}{
+		"graph with 3 vertices": {
+			graph: Graph[int]{
+				vertices: Set[int]{
+					1: 100,
+					2: 150,
+					3: 200,
+				},
+			},
+			expected: []int{100, 150, 200},
+		},
+	}
+
+	equals := func(a, b int) bool {
+		return a == b
+	}
+
+	for name, test := range tests {
+		actual := test.graph.Vertices()
+
+		if !slicesAreEqual(actual, test.expected, equals) {
+			t.Fatalf("%s: expected %v, got %v", name, test.expected, actual)
+		}
+	}
+}
+
+func TestGraph_Edges(t *testing.T) {
+	tests := map[string]struct {
+		graph    Graph[int]
+		expected []Pair[int]
+	}{
+		"graph with 3 edges": {
+			graph: Graph[int]{
+				edges: Set[Pair[int]]{
+					Pair[int]{A: 100, B: 150}: Pair[int]{A: 100, B: 150},
+					Pair[int]{A: 150, B: 200}: Pair[int]{A: 150, B: 200},
+					Pair[int]{A: 200, B: 100}: Pair[int]{A: 200, B: 100},
+				},
+			},
+			expected: []Pair[int]{
+				{A: 100, B: 150},
+				{A: 150, B: 200},
+				{A: 200, B: 100},
+			},
+		},
+	}
+
+	equals := func(a, b Pair[int]) bool {
+		return a.Equals(b)
+	}
+
+	for name, test := range tests {
+		actual := test.graph.Edges()
+
+		if !slicesAreEqual(actual, test.expected, equals) {
+			t.Fatalf("%s: expected %v, got %v", name, test.expected, actual)
+		}
+	}
+}
+
+func slicesAreEqual[T any](a []T, b []T, equals func(a, b T) bool) bool {
 	if len(a) != len(b) {
 		return false
 	}
@@ -38,7 +106,7 @@ func slicesAreEqual[T comparable](a []T, b []T) bool {
 	for _, aValue := range a {
 		found := false
 		for _, bValue := range b {
-			if aValue == bValue {
+			if equals(aValue, bValue) {
 				found = true
 			}
 		}

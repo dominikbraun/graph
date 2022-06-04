3ca553bcd78cef106cc162acfd667e1bbd8abd77
Dominik Braun <dominik.braun@chefkoch.de>
2022-06-04 23:11:18 +0200

    Implement and test functions for adding vertices and edges

--- graph.go
+++ graph.go
@@ -1,15 +1,26 @@
 package graph
 
+import "fmt"
+
 type Graph[K comparable, T any] struct {
 	hash       Hash[K, T]
 	properties *Properties
 	vertices   map[K]T
+	edges      map[K]map[K]Edge[T]
+}
+
+type Edge[T any] struct {
+	Source T
+	Target T
+	Weight int
 }
 
 func New[K comparable, T any](hash Hash[K, T], options ...func(*Properties)) *Graph[K, T] {
 	g := Graph[K, T]{
 		hash:       hash,
 		properties: &Properties{},
+		vertices:   make(map[K]T),
+		edges:      make(map[K]map[K]Edge[T]),
 	}
 
 	for _, option := range options {
@@ -17,4 +28,50 @@ func New[K comparable, T any](hash Hash[K, T], options ...func(*Properties)) *Gr
 	}
 
 	return &g
+}
+
+func (g *Graph[K, T]) Vertex(value T) {
+	hash := g.hash(value)
+	g.vertices[hash] = value
+}
+
+func (g *Graph[K, T]) Edge(source, target T) {
+	g.WeightedEdge(source, target, 0)
+}
+
+func (g *Graph[K, T]) WeightedEdge(source, target T, weight int) {
+	edge := Edge[T]{
+		Source: source,
+		Target: target,
+		Weight: weight,
+	}
+
+	sourceHash := g.hash(source)
+	targetHash := g.hash(target)
+
+	if _, ok := g.edges[sourceHash]; !ok {
+		g.edges[sourceHash] = make(map[K]Edge[T])
+	}
+
+	g.edges[sourceHash][targetHash] = edge
+}
+
+func (g *Graph[K, T]) EdgeByHashes(sourceHash, targetHash K) error {
+	return g.WeightedEdgeByHashes(sourceHash, targetHash, 0)
+}
+
+func (g *Graph[K, T]) WeightedEdgeByHashes(sourceHash, targetHash K, weight int) error {
+	source, ok := g.vertices[sourceHash]
+	if !ok {
+		return fmt.Errorf("could not find source vertex with hash %v", source)
+	}
+
+	target, ok := g.vertices[targetHash]
+	if !ok {
+		return fmt.Errorf("could not find target vertex with hash %v", source)
+	}
+
+	g.WeightedEdge(source, target, weight)
+
+	return nil
 }
--- graph_test.go
+++ graph_test.go
@@ -1,5 +1,144 @@
 package graph
 
+import "testing"
+
+func TestGraph_Vertex(t *testing.T) {
+	tests := map[string]struct {
+		vertices         []int
+		expectedVertices []int
+	}{
+		"graph with 3 vertices": {
+			vertices:         []int{1, 2, 3},
+			expectedVertices: []int{1, 2, 3},
+		},
+		"graph with duplicated vertex": {
+			vertices:         []int{1, 2, 2},
+			expectedVertices: []int{1, 2},
+		},
+	}
+
+	graph := New(IntHash)
+
+	for name, test := range tests {
+		for _, vertex := range test.vertices {
+			graph.Vertex(vertex)
+		}
+
+		for _, vertex := range test.vertices {
+			hash := graph.hash(vertex)
+			if _, ok := graph.vertices[hash]; !ok {
+				t.Errorf("%s: vertex %v not found in graph: %v", name, vertex, graph.vertices)
+			}
+		}
+	}
+}
+
+func TestGraph_Edge(t *testing.T) {
+	TestGraph_WeightedEdge(t)
+}
+
+func TestGraph_WeightedEdge(t *testing.T) {
+	tests := map[string]struct {
+		vertices      []int
+		edges         [][3]int
+		expectedEdges []Edge[int]
+	}{
+		"graph with 2 edges": {
+			vertices: []int{1, 2, 3},
+			edges:    [][3]int{{1, 2, 10}, {1, 3, 20}},
+			expectedEdges: []Edge[int]{
+				{Source: 1, Target: 2, Weight: 10},
+				{Source: 1, Target: 3, Weight: 20},
+			},
+		},
+	}
+
+	graph := New(IntHash)
+
+	for name, test := range tests {
+		for _, vertex := range test.vertices {
+			graph.Vertex(vertex)
+		}
+		for _, edge := range test.edges {
+			graph.WeightedEdge(edge[0], edge[1], edge[2])
+		}
+		for _, expectedEdge := range test.expectedEdges {
+			sourceHash := graph.hash(expectedEdge.Source)
+			targetHash := graph.hash(expectedEdge.Target)
+
+			edge, ok := graph.edges[sourceHash][targetHash]
+			if !ok {
+				t.Fatalf("%s: edge with source %v and target %v not found", name, expectedEdge.Source, expectedEdge.Target)
+			}
+
+			if edge.Source != expectedEdge.Source {
+				t.Errorf("%s: edge sources don't match: expected source %v, got %v", name, expectedEdge.Source, edge.Source)
+			}
+
+			if edge.Target != expectedEdge.Target {
+				t.Errorf("%s: edge targets don't match: expected target %v, got %v", name, expectedEdge.Target, edge.Target)
+			}
+
+			if edge.Weight != expectedEdge.Weight {
+				t.Errorf("%s: edge weights don't match: expected weight %v, got %v", name, expectedEdge.Weight, edge.Weight)
+			}
+		}
+	}
+}
+
+func TestGraph_EdgeByHashes(t *testing.T) {
+	TestGraph_WeightedEdgeByHashes(t)
+}
+
+func TestGraph_WeightedEdgeByHashes(t *testing.T) {
+	tests := map[string]struct {
+		vertices      []int
+		edgeHashes    [][3]int
+		expectedEdges []Edge[int]
+	}{
+		"graph with 2 edges": {
+			vertices:   []int{1, 2, 3},
+			edgeHashes: [][3]int{{1, 2, 10}, {1, 3, 20}},
+			expectedEdges: []Edge[int]{
+				{Source: 1, Target: 2, Weight: 10},
+				{Source: 1, Target: 3, Weight: 20},
+			},
+		},
+	}
+
+	graph := New(IntHash)
+
+	for name, test := range tests {
+		for _, vertex := range test.vertices {
+			graph.Vertex(vertex)
+		}
+		for _, edge := range test.edgeHashes {
+			graph.WeightedEdgeByHashes(edge[0], edge[1], edge[2])
+		}
+		for _, expectedEdge := range test.expectedEdges {
+			sourceHash := graph.hash(expectedEdge.Source)
+			targetHash := graph.hash(expectedEdge.Target)
+
+			edge, ok := graph.edges[sourceHash][targetHash]
+			if !ok {
+				t.Fatalf("%s: edge with source %v and target %v not found", name, expectedEdge.Source, expectedEdge.Target)
+			}
+
+			if edge.Source != expectedEdge.Source {
+				t.Errorf("%s: edge sources don't match: expected source %v, got %v", name, expectedEdge.Source, edge.Source)
+			}
+
+			if edge.Target != expectedEdge.Target {
+				t.Errorf("%s: edge targets don't match: expected target %v, got %v", name, expectedEdge.Target, edge.Target)
+			}
+
+			if edge.Weight != expectedEdge.Weight {
+				t.Errorf("%s: edge weights don't match: expected weight %v, got %v", name, expectedEdge.Weight, edge.Weight)
+			}
+		}
+	}
+}
+
 func slicesAreEqual[T any](a []T, b []T, equals func(a, b T) bool) bool {
 	if len(a) != len(b) {
 		return false

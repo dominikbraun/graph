ff177e7db2c780c19d4fb96b179f811d0629e2cc
Dominik Braun <dominik.braun@chefkoch.de>
2022-06-15 22:15:31 +0200

    Make Graph[K, T] an interface and provide directed and undirected implementations

---
+++ undirected.go
@@ -0,0 +1,215 @@
+package graph
+
+import "fmt"
+
+type undirected[K comparable, T any] struct {
+	hash       Hash[K, T]
+	properties *properties
+	vertices   map[K]T
+	edges      map[K]map[K]Edge[T]
+	outEdges   map[K]map[K]Edge[T]
+	inEdges    map[K]map[K]Edge[T]
+}
+
+func newUndirected[K comparable, T any](hash Hash[K, T], properties *properties) *undirected[K, T] {
+	return &undirected[K, T]{
+		hash:       hash,
+		properties: properties,
+		vertices:   make(map[K]T),
+		edges:      make(map[K]map[K]Edge[T]),
+		outEdges:   make(map[K]map[K]Edge[T]),
+		inEdges:    make(map[K]map[K]Edge[T]),
+	}
+}
+
+func (u *undirected[K, T]) Vertex(value T) {
+	hash := u.hash(value)
+	u.vertices[hash] = value
+}
+
+func (u *undirected[K, T]) Edge(source, target T) error {
+	return u.WeightedEdge(source, target, 0)
+}
+
+func (u *undirected[K, T]) WeightedEdge(source, target T, weight int) error {
+	sourceHash := u.hash(source)
+	targetHash := u.hash(target)
+
+	return u.WeightedEdgeByHashes(sourceHash, targetHash, weight)
+}
+
+func (u *undirected[K, T]) EdgeByHashes(sourceHash, targetHash K) error {
+	return u.WeightedEdgeByHashes(sourceHash, targetHash, 0)
+}
+
+func (u *undirected[K, T]) WeightedEdgeByHashes(sourceHash, targetHash K, weight int) error {
+	source, ok := u.vertices[sourceHash]
+	if !ok {
+		return fmt.Errorf("could not find source vertex with hash %v", sourceHash)
+	}
+
+	target, ok := u.vertices[targetHash]
+	if !ok {
+		return fmt.Errorf("could not find target vertex with hash %v", targetHash)
+	}
+
+	if _, ok := u.GetEdgeByHashes(sourceHash, targetHash); ok {
+		return fmt.Errorf("an edge between vertices %v and %v already exists", sourceHash, targetHash)
+	}
+
+	edge := Edge[T]{
+		Source: source,
+		Target: target,
+		Weight: weight,
+	}
+
+	u.addEdge(sourceHash, targetHash, edge)
+
+	return nil
+}
+
+func (u *undirected[K, T]) GetEdge(source, target T) (Edge[T], bool) {
+	sourceHash := u.hash(source)
+	targetHash := u.hash(target)
+
+	return u.GetEdgeByHashes(sourceHash, targetHash)
+}
+
+func (u *undirected[K, T]) GetEdgeByHashes(sourceHash, targetHash K) (Edge[T], bool) {
+	sourceEdges, ok := u.edges[sourceHash]
+	if !ok && u.properties.isDirected {
+		return Edge[T]{}, false
+	}
+
+	if edge, ok := sourceEdges[targetHash]; ok {
+		return edge, true
+	}
+
+	if !u.properties.isDirected {
+		targetEdges, ok := u.edges[targetHash]
+		if !ok {
+			return Edge[T]{}, false
+		}
+
+		if edge, ok := targetEdges[sourceHash]; ok {
+			return edge, true
+		}
+	}
+
+	return Edge[T]{}, false
+}
+
+func (u *undirected[K, T]) DFS(start T, visit func(value T) bool) error {
+	startHash := u.hash(start)
+
+	return u.DFSByHash(startHash, visit)
+}
+
+func (u *undirected[K, T]) DFSByHash(startHash K, visit func(value T) bool) error {
+	if _, ok := u.vertices[startHash]; !ok {
+		return fmt.Errorf("could not find start vertex with hash %v", startHash)
+	}
+
+	stack := make([]K, 0)
+	visited := make(map[K]bool)
+
+	stack = append(stack, startHash)
+
+	for len(stack) > 0 {
+		currentHash := stack[len(stack)-1]
+		currentVertex := u.vertices[currentHash]
+
+		stack = stack[:len(stack)-1]
+
+		if _, ok := visited[currentHash]; !ok {
+			// Stop traversing the graph if the visit function returns true.
+			if visit(currentVertex) {
+				break
+			}
+			visited[currentHash] = true
+
+			for adjacency := range u.outEdges[currentHash] {
+				stack = append(stack, adjacency)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (u *undirected[K, T]) BFS(start T, visit func(value T) bool) error {
+	startHash := u.hash(start)
+
+	return u.BFSByHash(startHash, visit)
+}
+
+func (u *undirected[K, T]) BFSByHash(startHash K, visit func(value T) bool) error {
+	if _, ok := u.vertices[startHash]; !ok {
+		return fmt.Errorf("could not find start vertex with hash %v", startHash)
+	}
+
+	queue := make([]K, 0)
+	visited := make(map[K]bool)
+
+	visited[startHash] = true
+	queue = append(queue, startHash)
+
+	for len(queue) > 0 {
+		currentHash := queue[0]
+		currentVertex := u.vertices[currentHash]
+
+		queue = queue[1:]
+
+		// Stop traversing the graph if the visit function returns true.
+		if visit(currentVertex) {
+			break
+		}
+
+		for adjacency := range u.outEdges[currentHash] {
+			if _, ok := visited[adjacency]; !ok {
+				visited[adjacency] = true
+				queue = append(queue, adjacency)
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (u *undirected[K, T]) edgesAreEqual(a, b Edge[T]) bool {
+	aSourceHash := u.hash(a.Source)
+	aTargetHash := u.hash(a.Target)
+	bSourceHash := u.hash(b.Source)
+	bTargetHash := u.hash(b.Target)
+
+	if aSourceHash == bSourceHash && aTargetHash == bTargetHash {
+		return true
+	}
+
+	if !u.properties.isDirected {
+		return aSourceHash == bTargetHash && aTargetHash == bSourceHash
+	}
+
+	return false
+}
+
+func (u *undirected[K, T]) addEdge(sourceHash, targetHash K, edge Edge[T]) {
+	if _, ok := u.edges[sourceHash]; !ok {
+		u.edges[sourceHash] = make(map[K]Edge[T])
+	}
+
+	u.edges[sourceHash][targetHash] = edge
+
+	if _, ok := u.outEdges[sourceHash]; !ok {
+		u.outEdges[sourceHash] = make(map[K]Edge[T])
+	}
+
+	u.outEdges[sourceHash][targetHash] = edge
+
+	if _, ok := u.inEdges[targetHash]; !ok {
+		u.inEdges[targetHash] = make(map[K]Edge[T])
+	}
+
+	u.inEdges[targetHash][sourceHash] = edge
+}

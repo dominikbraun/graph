52dddab9da3705750aa33dc9952930c672b2258d
Dominik Braun <mail@dominikbraun.io>
2023-04-22 20:10:46 +0200

    Implement MaximumSpanningTree

--- trees.go
+++ trees.go
@@ -7,6 +7,14 @@ import (
 )
 
 func MinimumSpanningTree[K comparable, T any](g Graph[K, T]) (Graph[K, T], error) {
+	return spanningTree(g, false)
+}
+
+func MaximumSpanningTree[K comparable, T any](g Graph[K, T]) (Graph[K, T], error) {
+	return spanningTree(g, true)
+}
+
+func spanningTree[K comparable, T any](g Graph[K, T], maximum bool) (Graph[K, T], error) {
 	if g.Traits().IsDirected {
 		return nil, errors.New("spanning trees can only be determined for undirected graphs")
 	}
@@ -22,8 +30,6 @@ func MinimumSpanningTree[K comparable, T any](g Graph[K, T]) (Graph[K, T], error
 	mst := New(g.(*undirected[K, T]).hash)
 
 	for v, adjacencies := range adjacencyMap {
-		subtrees.add(v)
-
 		vertex, properties, err := g.VertexWithProperties(v)
 		if err != nil {
 			return nil, fmt.Errorf("failed to get vertex %v: %w", v, err)
@@ -34,14 +40,22 @@ func MinimumSpanningTree[K comparable, T any](g Graph[K, T]) (Graph[K, T], error
 			return nil, fmt.Errorf("failed to add vertex %v: %w", v, err)
 		}
 
+		subtrees.add(v)
+
 		for _, edge := range adjacencies {
 			edges = append(edges, edge)
 		}
 	}
 
-	sort.Slice(edges, func(i, j int) bool {
-		return edges[i].Properties.Weight < edges[j].Properties.Weight
-	})
+	if maximum {
+		sort.Slice(edges, func(i, j int) bool {
+			return edges[i].Properties.Weight > edges[j].Properties.Weight
+		})
+	} else {
+		sort.Slice(edges, func(i, j int) bool {
+			return edges[i].Properties.Weight < edges[j].Properties.Weight
+		})
+	}
 
 	for _, edge := range edges {
 		sourceRoot := subtrees.find(edge.Source)
--- trees_test.go
+++ trees_test.go
@@ -4,6 +4,28 @@ import (
 	"testing"
 )
 
+func TestDirectedMinimumSpanningTree(t *testing.T) {
+	tests := map[string]struct {
+		shouldFail bool
+	}{
+		"returns error": {
+			shouldFail: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			graph := New(IntHash, Directed())
+
+			_, err := MinimumSpanningTree(graph)
+
+			if test.shouldFail != (err != nil) {
+				t.Errorf("expected error == %v, got %v", test.shouldFail, err)
+			}
+		})
+	}
+}
+
 func TestUndirectedMinimumSpanningTree(t *testing.T) {
 	tests := map[string]struct {
 		vertices                []string
@@ -54,6 +76,123 @@ func TestUndirectedMinimumSpanningTree(t *testing.T) {
 			}
 
 			mst, _ := MinimumSpanningTree(g)
+			adjacencyMap, _ := mst.AdjacencyMap()
+
+			for expectedHash, expectedAdjacencies := range test.expectedMSTAdjacencyMap {
+				actualAdjacencies, ok := adjacencyMap[expectedHash]
+				if !ok {
+					t.Errorf("%s: key %v doesn't exist in adjacency map", name, expectedHash)
+					continue
+				}
+
+				for expectedAdjacency, expectedEdge := range expectedAdjacencies {
+					actualEdge, ok := actualAdjacencies[expectedAdjacency]
+					if !ok {
+						t.Errorf("%s: key %v doesn't exist in adjacencies of %v", name, expectedAdjacency, expectedHash)
+						continue
+					}
+
+					if !mst.(*undirected[string, string]).edgesAreEqual(expectedEdge, actualEdge) {
+						t.Errorf("%s: expected edge %v, got %v at AdjacencyMap[%v][%v]", name, expectedEdge, actualEdge, expectedHash, expectedAdjacency)
+					}
+
+					for expectedKey, expectedValue := range expectedEdge.Properties.Attributes {
+						actualValue, ok := actualEdge.Properties.Attributes[expectedKey]
+						if !ok {
+							t.Errorf("%s: expected attribute %v to exist in edge %v", name, expectedKey, actualEdge)
+						}
+						if actualValue != expectedValue {
+							t.Errorf("%s: expected value %v for key %v in edge %v, got %v", name, expectedValue, expectedKey, expectedEdge, actualValue)
+						}
+					}
+
+					if actualEdge.Properties.Weight != expectedEdge.Properties.Weight {
+						t.Errorf("%s: expected weight %v for edge %v, got %v", name, expectedEdge.Properties.Weight, expectedEdge, actualEdge.Properties.Weight)
+					}
+				}
+			}
+
+			for actualHash := range adjacencyMap {
+				if _, ok := test.expectedMSTAdjacencyMap[actualHash]; !ok {
+					t.Errorf("%s: unexpected key %v in union adjacency map", name, actualHash)
+				}
+			}
+		})
+	}
+}
+
+func TestDirectedMaximumSpanningTree(t *testing.T) {
+	tests := map[string]struct {
+		shouldFail bool
+	}{
+		"returns error": {
+			shouldFail: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			graph := New(IntHash, Directed())
+
+			_, err := MaximumSpanningTree(graph)
+
+			if test.shouldFail != (err != nil) {
+				t.Errorf("expected error == %v, got %v", test.shouldFail, err)
+			}
+		})
+	}
+}
+
+func TestUndirectedMaximumSpanningTree(t *testing.T) {
+	tests := map[string]struct {
+		vertices                []string
+		edges                   []Edge[string]
+		expectedErr             error
+		expectedMSTAdjacencyMap map[string]map[string]Edge[string]
+	}{
+		"graph from img/mst.svg with higher weights": {
+			vertices: []string{"A", "B", "C", "D"},
+			edges: []Edge[string]{
+				{Source: "A", Target: "B", Properties: EdgeProperties{Weight: 20}},
+				{Source: "A", Target: "C", Properties: EdgeProperties{Weight: 4}},
+				{Source: "A", Target: "D", Properties: EdgeProperties{Weight: 3}},
+				{Source: "B", Target: "C", Properties: EdgeProperties{Weight: 4}},
+				{Source: "B", Target: "D", Properties: EdgeProperties{Weight: 10}},
+				{Source: "C", Target: "D", Properties: EdgeProperties{Weight: 30}},
+			},
+			expectedErr: nil,
+			expectedMSTAdjacencyMap: map[string]map[string]Edge[string]{
+				"A": {
+					"B": {Source: "A", Target: "B", Properties: EdgeProperties{Weight: 20}},
+				},
+				"B": {
+					"D": {Source: "B", Target: "D", Properties: EdgeProperties{Weight: 10}},
+					"A": {Source: "B", Target: "A", Properties: EdgeProperties{Weight: 20}},
+				},
+				"C": {
+					"D": {Source: "C", Target: "D", Properties: EdgeProperties{Weight: 30}},
+				},
+				"D": {
+					"B": {Source: "D", Target: "B", Properties: EdgeProperties{Weight: 10}},
+					"C": {Source: "D", Target: "C", Properties: EdgeProperties{Weight: 30}},
+				},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			g := New(StringHash)
+
+			for _, vertex := range test.vertices {
+				_ = g.AddVertex(vertex)
+			}
+
+			for _, edge := range test.edges {
+				_ = g.AddEdge(edge.Source, edge.Target, copyEdgeProperties(edge.Properties))
+			}
+
+			mst, _ := MaximumSpanningTree(g)
 			adjacencyMap, _ := mst.AdjacencyMap()
 
 			for expectedHash, expectedAdjacencies := range test.expectedMSTAdjacencyMap {

f9db0b1082740dd9e2ecaa15d2c20e8980c7df5b
Jon Brandenburg <jbrandenburg@yahoo.com>
2023-07-15 16:37:04 -0400

    enhancement: created verifyTopologicalSort function

--- dag_test.go
+++ dag_test.go
@@ -52,20 +52,9 @@ func TestDirectedTopologicalSort(t *testing.T) {
 	for name, test := range tests {
 		graph := New(IntHash, Directed())
 
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(test.vertices), func(i, j int) { test.vertices[i], test.vertices[j] = test.vertices[j], test.vertices[i] })
-
-		for _, vertex := range test.vertices {
-			_ = graph.AddVertex(vertex)
-		}
-
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(test.edges), func(i, j int) { test.edges[i], test.edges[j] = test.edges[j], test.edges[i] })
-
-		for _, edge := range test.edges {
-			if err := graph.AddEdge(edge.Source, edge.Target, EdgeWeight(edge.Properties.Weight)); err != nil {
-				t.Fatalf("%s: failed to add edge: %s", name, err.Error())
-			}
+		err := buildGraph(&graph, test.vertices, test.edges)
+		if err != nil {
+			t.Fatalf("%s: failed to construct graph: %s", name, err.Error())
 		}
 
 		order, err := TopologicalSort(graph)
@@ -86,27 +75,8 @@ func TestDirectedTopologicalSort(t *testing.T) {
 
 			fmt.Println("topological sort", order)
 
-			adjacencyMap, err := graph.AdjacencyMap()
-			if err != nil {
-				t.Errorf("%s: invalid topological sort - failed to get adjacency map: %v", name, err)
-				return
-			}
-
-			for i := range order {
-
-				for _, edge := range adjacencyMap[order[i]] {
-					err = graph.RemoveEdge(edge.Source, edge.Target)
-					if err != nil {
-						t.Errorf("%s: invalid topological sort - failed to remove edge: %d -> %d", name, edge.Source, edge.Target)
-						return
-					}
-				}
-
-				err = graph.RemoveVertex(order[i])
-				if err == ErrVertexHasEdges {
-					t.Errorf("%s: invalid topological sort - failed to remove vertex: %d at index %d", name, order[i], i)
-					return
-				}
+			if err := verifyTopologicalSort(graph, order); err != nil {
+				t.Errorf("%s: invalid topological sort - %v", name, err)
 			}
 		}
 
@@ -190,20 +160,9 @@ func TestDirectedStableTopologicalSort(t *testing.T) {
 	for name, test := range tests {
 		graph := New(IntHash, Directed())
 
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(test.vertices), func(i, j int) { test.vertices[i], test.vertices[j] = test.vertices[j], test.vertices[i] })
-
-		for _, vertex := range test.vertices {
-			_ = graph.AddVertex(vertex)
-		}
-
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(test.edges), func(i, j int) { test.edges[i], test.edges[j] = test.edges[j], test.edges[i] })
-
-		for _, edge := range test.edges {
-			if err := graph.AddEdge(edge.Source, edge.Target, EdgeWeight(edge.Properties.Weight)); err != nil {
-				t.Fatalf("%s: failed to add edge: %s", name, err.Error())
-			}
+		err := buildGraph(&graph, test.vertices, test.edges)
+		if err != nil {
+			t.Fatalf("%s: failed to construct graph: %s", name, err.Error())
 		}
 
 		order, err := StableTopologicalSort(graph, func(a, b int) bool {
@@ -299,14 +258,9 @@ func TestDirectedTransitiveReduction(t *testing.T) {
 	for name, test := range tests {
 		graph := New(StringHash, Directed())
 
-		for _, vertex := range test.vertices {
-			_ = graph.AddVertex(vertex)
-		}
-
-		for _, edge := range test.edges {
-			if err := graph.AddEdge(edge.Source, edge.Target, EdgeWeight(edge.Properties.Weight)); err != nil {
-				t.Fatalf("%s: failed to add edge: %s", name, err.Error())
-			}
+		err := buildGraph(&graph, test.vertices, test.edges)
+		if err != nil {
+			t.Fatalf("%s: failed to construct graph: %s", name, err.Error())
 		}
 
 		reduction, err := TransitiveReduction(graph)
@@ -356,6 +310,103 @@ func TestUndirectedTransitiveReduction(t *testing.T) {
 	}
 }
 
+func TestVerifyTopologicalSort(t *testing.T) {
+	tests := map[string]struct {
+		vertices      []int
+		edges         []Edge[int]
+		invalidOrder []int
+	}{
+		"graph with 2 vertices": {
+			vertices: []int{1, 2},
+			edges: []Edge[int]{
+				{Source: 1, Target: 2},
+			},
+		},
+		"graph with 2 vertices - reversed": {
+			vertices: []int{1, 2},
+			edges: []Edge[int]{
+				{Source: 2, Target: 1},
+			},
+		},
+		"graph with 2 vertices - invalid": {
+			vertices: []int{1, 2},
+			edges: []Edge[int]{
+				{Source: 1, Target: 2},
+			},
+			invalidOrder: []int{2, 1},
+		},
+		"graph with 3 vertices": {
+			vertices: []int{1, 2, 3},
+			edges: []Edge[int]{
+				{Source: 1, Target: 2},
+				{Source: 1, Target: 3},
+				{Source: 2, Target: 3},
+			},
+		},
+		"graph with 3 vertices - invalid": {
+			vertices: []int{1, 2, 3},
+			edges: []Edge[int]{
+				{Source: 1, Target: 2},
+				{Source: 1, Target: 3},
+				{Source: 2, Target: 3},
+			},
+			invalidOrder: []int{1, 3, 2},
+		},
+		"graph with 5 vertices": {
+			vertices: []int{1, 2, 3, 4, 5},
+			edges: []Edge[int]{
+				{Source: 1, Target: 2},
+				{Source: 1, Target: 3},
+				{Source: 2, Target: 3},
+				{Source: 2, Target: 4},
+				{Source: 2, Target: 5},
+				{Source: 3, Target: 4},
+				{Source: 4, Target: 5},
+			},
+		},
+		"graph with many possible topological orders": {
+			vertices: []int{1, 2, 3, 4, 5, 6, 10, 20, 30, 40, 50, 60},
+			edges: []Edge[int]{
+				{Source: 1, Target: 10},
+				{Source: 2, Target: 20},
+				{Source: 3, Target: 30},
+				{Source: 4, Target: 40},
+				{Source: 5, Target: 50},
+				{Source: 6, Target: 60},
+			},
+			invalidOrder: []int{2, 3, 4, 5, 6, 10, 1, 20, 30, 40, 50, 60},
+		},
+	}
+
+	for name, test := range tests {
+		graph := New[int, int](IntHash, Directed())
+
+		err := buildGraph(&graph, test.vertices, test.edges)
+		if err != nil {
+			t.Fatalf("%s: failed to construct graph: %s", name, err.Error())
+		}
+
+		var order[] int
+
+		if len(test.invalidOrder) > 0 {
+			order = test.invalidOrder
+		} else {
+			order, err = TopologicalSort(graph)
+			if err != nil {
+				t.Fatalf("%s: error failed to produce topological sort: %v)", name, err)
+			}
+		}
+
+		err = verifyTopologicalSort(graph, order)
+
+		shouldFail := len(test.invalidOrder) > 0
+
+		if shouldFail != (err != nil) {
+			t.Errorf("%s: error expectancy doesn't match: expected %v, got %v (error: %v)", name, shouldFail, err != nil, err)
+		}
+	}
+}
+
 func slicesAreEqualWithFunc[T any](a, b []T, equals func(a, b T) bool) bool {
 	if len(a) != len(b) {
 		return false
@@ -374,4 +425,57 @@ func slicesAreEqualWithFunc[T any](a, b []T, equals func(a, b T) bool) bool {
 	}
 
 	return true
+}
+
+// Please note that this call is destructive.  Make a clone of your graph before calling if you
+// wish to preserve the graph.
+func verifyTopologicalSort[K comparable, T any](graph Graph[K, T], order []K) error {
+
+	adjacencyMap, err := graph.AdjacencyMap()
+	if err != nil {
+		return fmt.Errorf("failed to get adjacency map: %v", err)
+	}
+
+	for i := range order {
+
+		for _, edge := range adjacencyMap[order[i]] {
+			err = graph.RemoveEdge(edge.Source, edge.Target)
+			if err != nil {
+				return fmt.Errorf("failed to remove edge: %v -> %v : %v", edge.Source, edge.Target, err)
+			}
+		}
+
+		err = graph.RemoveVertex(order[i])
+		if err != nil {
+			return fmt.Errorf("failed to remove vertex: %v at index %d: %v", order[i], i, err)
+		}
+	}
+
+	return nil
+}
+
+// randomizes the ordering of the edges and vertices to help ferret out any potential bugs
+// related to ordering
+func buildGraph[K comparable, T any](g *Graph[K, T], vertices []T, edges []Edge[K]) error {
+
+	if g == nil {
+		return fmt.Errorf("graph must be initialized")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(vertices), func(i, j int) { vertices[i], vertices[j] = vertices[j], vertices[i] })
+
+	for _, vertex := range vertices {
+		_ = (*g).AddVertex(vertex)
+	}
+
+	rand.Shuffle(len(edges), func(i, j int) { edges[i], edges[j] = edges[j], edges[i] })
+
+	for _, edge := range edges {
+		if err := (*g).AddEdge(edge.Source, edge.Target, EdgeWeight(edge.Properties.Weight)); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
